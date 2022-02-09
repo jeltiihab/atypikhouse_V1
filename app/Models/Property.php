6eb44dd6bdd7c5a4436dfbef4ec59f57bae07c48
jeltiihab <ihab.jelti@gmail.com>
2022-02-09 12:30:19 +0100

    adding approving property by admin

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,30 +2,26 @@
 
 namespace App\Models;
 
+use App\Scopes\HasActiveScope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Kirschbaum\PowerJoins\PowerJoins;
-use KirschbaumDevelopment\NovaComments\Commentable;
 
 class Property extends Model
 {
 
 
 
-    use HasFactory,SoftDeletes,PowerJoins,Commentable;
+    use HasFactory,SoftDeletes,PowerJoins;
 
-    public $casts = [
-
-        "dynamic_attributes"=>"json",
-        "equipments" =>"json"
-    ];
-
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         "user_id", "category_id", "name", "rooms", "description", "surface", "hosting_capacity", "check_in_at", "check_out_at", "price", "is_activated", "images", "equipments", "dynamic_attributes"
     ];
-
-
 
     public function address(): \Illuminate\Database\Eloquent\Relations\HasOne
     {
@@ -41,4 +37,18 @@ class Property extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        //add GlobalScope is_activated is true
+        static::addGlobalScope('is_activated', function (Builder $builder) {
+            $builder->where('is_activated', true);
+        });
+    }
+
 }
